323e226d863f3cbbda92028b612608425785cd2a
kate mwende <kmwende419@gmail.com>
2023-08-10 20:03:14 +0300

    Reset valid_email if email changes

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Reset valid_email before update on users table
+
+DELIMITER //
+
+CREATE TRIGGER email_reset BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+       SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER ;
+    
