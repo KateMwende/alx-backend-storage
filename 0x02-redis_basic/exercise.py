08d72e93a1d0902665f0b1e2cf29487e4f489b15
kate mwende <kmwende419@gmail.com>
2023-08-16 22:00:36 +0300

    Coversion of data type formats

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Module that creates redis classes and methods
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None
+            ) -> Union[str, bytes, int, float]:
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Return a string"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: int) -> int:
+        """Return an int"""
+        value = self._redis.get(key)
+        return int(value.decode("utf-8"))
